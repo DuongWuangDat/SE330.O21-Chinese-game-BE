74790937b4b78ab0a2e0e017b61ffe69cd43f0a6
DuongWuangDat <duongquangdat172004@gmail.com>
2024-04-08 19:22:15 +0700

    fix

--- src/main/java/com/chinese_chess_BE/config/SecurityConfiguration.java
+++ src/main/java/com/chinese_chess_BE/config/SecurityConfiguration.java
@@ -29,7 +29,8 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 public class SecurityConfiguration{
     private final String[] WHITE_LIST_URL = {
             "/api/v1/history/create",
-            "/api/v1/auth/**",
+            "/api/v1/auth/login",
+            "/api/v1/auth/register",
             "/api/v1/user/leaderboard"
     };
 
